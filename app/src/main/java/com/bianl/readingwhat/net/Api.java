344e958daeeb49664c695cd0ff662b92c87bc32e
fhbianling <fhbianling@163.com>
2016-11-12 17:59:23 +0800

    update

--- app/src/main/java/com/bianl/readingwhat/net/Api.java
+++ app/src/main/java/com/bianl/readingwhat/net/Api.java
@@ -5,10 +5,16 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
+import okio.BufferedSink;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -52,5 +58,4 @@ public class Api {
         }
         return request;
     }
-
 }
